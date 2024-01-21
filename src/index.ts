d47ab64ac5e70bd733a7520422d326ba2196779f
gouravrajbit <gouravr244@gmail.com>
2024-01-21 12:34:17 +0530

    Change path

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import cachingRoutes from './api/caching.api'
+import cacheRoutes from './api/cache.api'
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,7 +9,9 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 // caching routes
-app.use(cachingRoutes);
+app.use('/cache', cacheRoutes);
+
+
 
 app.listen(port, () => {
     console.log(`Server connected on ${port}!!`);
